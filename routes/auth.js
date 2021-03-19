a436c3a8e78919b6c5b611b1a73c02fd8eeacbe3
AJTech1968 <40666123+AJTech1968@users.noreply.github.com>
2021-03-19 23:04:13 +0530

    'initial'2

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ var router  = express.Router();
 
 const { signin, signup } = require('../controllers/auth');
 
-router.get('/signup', signup)
+router.post('/signup', signup)
 router.get('/signin', signin)
 
 module.exports = router;
