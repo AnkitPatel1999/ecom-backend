f5a41bfc76fceedd7e110e950178861ec6e7f94c
AJTech1968 <40666123+AJTech1968@users.noreply.github.com>
2021-04-29 10:26:31 +0530

    ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../models/UserModel");
+const User = require("../models/userModel");
 const Order = require("../models/orderModel");
 
 exports.getUserById = (req, res, next, id) => {
